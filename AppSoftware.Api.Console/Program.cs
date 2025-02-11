8b7f4c724ef9b2f9ed44145ccc96369add044d6d
Janiek Colpaert <janiek.colpaert@gmail.com>
2025-02-11 14:30:16 +0100

    Update Webclient to httpclient

--- AppSoftware.Api.Console/Program.cs
+++ AppSoftware.Api.Console/Program.cs
@@ -4,18 +4,22 @@ using AppSoftware.Api.Core;
 
 //using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 Console.WriteLine("Hello, Chuck Norris!");
 
 string url = "https://api.chucknorris.io/jokes/random";
 
-using (WebClient client = new WebClient())
+using (HttpClient client = new HttpClient())
 {
     try
     {
-        string response = client.DownloadString(url);
-        Joke joke = JsonSerializer.Deserialize<Joke>(response);
+        //HttpResponseMessage response = await client.GetAsync(url);
+        //string jsonString = await response.Content.ReadAsStringAsync();
+        //Joke joke = JsonSerializer.Deserialize<Joke>(jsonString);
+
+        var joke = await client.GetFromJsonAsync<Joke>(url);
         Console.WriteLine(joke.Value);
     }
     catch (WebException ex)
